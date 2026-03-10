eb6e6423549ecdcc5f7a8715258af17f3ea331ef
JLZMYD <jlzmyd@inf.elte.hu>
2026-03-10 01:47:24 +0100

    Create Quota clas, add Period enum

---
+++ gateway/models/sqlalchemy/quota.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import Enum as SQLAlchemyEnum
+from datetime import datetime
+from typing import Optional
+
+from gateway.models import SQLAlchemyBase, Status, Period
+
+class Quota(SQLAlchemyBase):
+    __tablename__ = "quotas"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    project_id : Mapped[int] = mapped_column(ForeignKey("projects.id"))
+    key_id : Mapped[int] = mapped_column(ForeignKey("api_keys.id"))
+    limit_id : Mapped[int] = mapped_column(ForeignKey("limits.id"))
+    limit_value : Mapped[int]
+    period : Mapped[Period] = mapped_column(SQLAlchemyEnum(Period))
+    expires_at : Mapped[Optional[datetime]]
+    status : Mapped[Status] = mapped_column(SQLAlchemyEnum(Status), default=Status.ACTIVE)
+    
