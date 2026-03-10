98d91172b9de3b09a6bc14ff67aaa0d2fb8eae2b
JLZMYD <jlzmyd@inf.elte.hu>
2026-03-10 01:22:04 +0100

    Move Status enum into its own file

--- gateway/models/sqlalchemy/project.py
+++ gateway/models/sqlalchemy/project.py
@@ -2,20 +2,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from datetime import datetime
 from typing import Optional
-from enum import Enum
 from sqlalchemy import Enum as SQLAlchemyEnum
 
-from gateway.models import SQLAlchemyBase
-
-class ProjectStatus(Enum):
-    ACTIVE = "active"
-    ARCHIVED = "archived"
+from gateway.models import SQLAlchemyBase, Status
 
 class Project(SQLAlchemyBase):
     __tablename__ = "projects"
 
     id : Mapped[int] = mapped_column(primary_key=True)
     name : Mapped[str] = mapped_column(String(150), unique=True)
-    status : Mapped[ProjectStatus] = mapped_column(SQLAlchemyEnum(ProjectStatus))
+    status : Mapped[Status] = mapped_column(SQLAlchemyEnum(Status), default=Status.ACTIVE)
     created_date : Mapped[datetime]
     modified_date : Mapped[Optional[datetime]]
