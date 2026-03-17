c99b12445df1ffc6958c1160253209aa9f974b07
JLZMYD <jlzmyd@inf.elte.hu>
2026-03-17 19:41:47 +0100

    Make db sessions async

--- shared/db/db.py
+++ shared/db/db.py
@@ -1,7 +1,7 @@
-from contextlib import contextmanager
-from typing import Generator
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from shared.config import SQLALCHEMY_DATABASE_URL
@@ -13,27 +13,18 @@ if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     _connect_args = {"check_same_thread": False}
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=_connect_args, future=True)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, connect_args=_connect_args, future=True)
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, class_=Session)
-
-
-def get_session() -> Generator[Session, None, None]:
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+AsyncSessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, class_=AsyncSession)
 
 
-@contextmanager
-def get_transactional_session() -> Generator[Session, None, None]:
-    session: Session = SessionLocal()
-    try:
+async def get_session() -> AsyncGenerator[AsyncSession, None, None]:
+    async with AsyncSessionLocal() as session:
         yield session
-        session.commit()
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+
+
+@asynccontextmanager
+async def get_transactional_session() -> AsyncGenerator[AsyncSession, None, None]:
+    async with AsyncSessionLocal() as session:
+        async with session.begin():
+            yield session
