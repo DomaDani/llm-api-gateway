12683cddc9276fa87c9977bc8b95df2e48500c7d
JLZMYD <jlzmyd@inf.elte.hu>
2026-03-18 18:06:43 +0100

    Increase request_id maximum length

--- shared/models/sqlalchemy/usage_log.py
+++ shared/models/sqlalchemy/usage_log.py
@@ -13,7 +13,7 @@ class UsageLog(SQLAlchemyBase):
     key_id: Mapped[int] = mapped_column(ForeignKey("api_keys.id"))
     project_id: Mapped[int] = mapped_column(ForeignKey("projects.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    request_id: Mapped[Optional[str]] = mapped_column(String(36), unique=True)
+    request_id: Mapped[Optional[str]] = mapped_column(String(255), unique=True)
     timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     request_type: Mapped[Optional[str]] = mapped_column(String(50))
