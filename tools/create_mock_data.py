5a273a2832566b025f9ab837ed4bf4d2a64130e5
JLZMYD <jlzmyd@inf.elte.hu>
2026-03-25 20:07:31 +0100

    Update mock data for integration tests

--- tools/create_mock_data.py
+++ tools/create_mock_data.py
@@ -73,17 +73,27 @@ async def create_mock_data():
 
         session.add_all([project_permission1, project_permission2])
 
-        api_key = APIKey(
+        api_key1 = APIKey(
             project=project,
             user=user2,
-            name="Test API Key",
+            name="End to end API key",
             fingerprint="TTcj1lxYOY9d",
             key_hash=hash_key("TTcj1lxYOY9dB25bVh6IKfOrwW8ERIWHXJKqxYYwxHM-_LHTf3isqFhitJxpVGiZaLf2GVwiKsQZLhnR-xYJ2Q"),
             create_date=datetime.now(timezone.utc),
             status=Status.ACTIVE
         )
 
-        session.add(api_key)
+        api_key2 = APIKey(
+            project=project,
+            user=user2,
+            name="Limited API key",
+            fingerprint="WQC-GPp6L8gl",
+            key_hash=hash_key("WQC-GPp6L8glbHgIqAmyTQjwwr6mOV3Ar3uuWG33x0j_HzGsD-7DxujaCl-EzkTkQwZBWSekQwvtnw-WNUKnDQ"),
+            create_date=datetime.now(timezone.utc),
+            status=Status.ACTIVE
+        )
+
+        session.add_all([api_key1, api_key2])
 
         limit = Limit(
             name="Token Limit",
@@ -93,10 +103,10 @@ async def create_mock_data():
         session.add(limit)
 
         quota = Quota(
-            api_key=api_key,
+            api_key=api_key2,
             limit=limit,
-            limit_value=5000,
-            period=Period.MINUTE,
+            limit_value=1000,
+            period=Period.HOUR,
             status=Status.ACTIVE
         )
 
