2c35e188b7a24f12fba75481d230675626e48e25
JLZMYD <jlzmyd@inf.elte.hu>
2026-04-01 23:58:29 +0200

    properly cast est_tokens

--- gateway/limits/quota.py
+++ gateway/limits/quota.py
@@ -1,3 +1,5 @@
+import math
+
 from fastapi import Depends, HTTPException
 from genai_prices import Usage, calc_price
 
@@ -12,7 +14,7 @@ from shared.config import DEFAULT_MAX_COMPLETION_TOKENS, QUOTA_STRICTNESS, PROVI
 async def check_limits_costs(body: OpenAIRequest, key_info: APIKey = Depends(validate_api_key)) -> ValidatedRequest:
     # est_input_tokens = await run_in_threadpool(get_token_count, body.messages)
     est_input_tokens = get_token_count(body.messages)
-    est_completion_tokens = (body.max_completion_tokens or DEFAULT_MAX_COMPLETION_TOKENS) * QUOTA_STRICTNESS
+    est_completion_tokens = int(math.ceil((body.max_completion_tokens or DEFAULT_MAX_COMPLETION_TOKENS) * QUOTA_STRICTNESS))
     estimated_total_tokens = est_input_tokens + est_completion_tokens
 
     estimated_cost = calc_price(
