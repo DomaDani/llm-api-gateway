3fccdbafa1ebc19e6314f5b176c3cc7059034955
JLZMYD <jlzmyd@inf.elte.hu>
2026-04-07 18:48:32 +0200

    move pages back under authprovider

--- dashboard/frontend/src/app/App.jsx
+++ dashboard/frontend/src/app/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from '../components/blocks/Sidebar';
+import { AuthProvider } from '../auth/AuthProvider';
+import ProtectedRoute from '../components/shared/ProtectedRoute';
 
+import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home'
 import GlobalSettings from '../pages/Global/GlobalSettings';
 import ProjectSetings from '../pages/Project/ProjectSettings';
@@ -10,37 +13,26 @@ import Profile from '../pages/Profile/Profile';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<Sidebar><Home /></Sidebar>} />
-      <Route path="/global"  element={<Sidebar><GlobalSettings /></Sidebar>} />
-      <Route path="/project"  element={<Sidebar><ProjectSetings /></Sidebar>} />
-      <Route path="/keys"   element={<Sidebar><ApiKeys /></Sidebar>} />
-      {/* <Route path="/statistics"   element={<Sidebar></Sidebar>} /> */}
-      <Route path="/usage"   element={<Sidebar><Usage /></Sidebar>} />
-      <Route path="/profile"   element={<Sidebar><Profile /></Sidebar>} />
-    </Routes>
+    <AuthProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
 
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Sidebar><Home /></Sidebar>} />
+            <Route path="/global"  element={<Sidebar><GlobalSettings /></Sidebar>} />
+            <Route path="/project"  element={<Sidebar><ProjectSetings /></Sidebar>} />
+            <Route path="/keys"   element={<Sidebar><ApiKeys /></Sidebar>} />
+            {/* <Route path="/statistics"   element={<Sidebar></Sidebar>} /> */}
+            <Route path="/usage"   element={<Sidebar><Usage /></Sidebar>} />
+            <Route path="/profile"   element={<Sidebar><Profile /></Sidebar>} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   )
 };
-
-/*
-
-    <>
-      <AuthProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element = {<Login />}/>
-
-            <Route element={<ProtectedRoute/>}>
-              <Route path="/welcome" element = {<Welcome />}/>
-            </Route>
-            
-          </Routes>
-        </div>
-      </AuthProvider>
-    </>
-
-*/
 
 export default App;
