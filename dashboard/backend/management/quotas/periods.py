e26162538cd0a59656b83602da896506dd214ae6
JLZMYD <jlzmyd@inf.elte.hu>
2026-04-10 21:52:45 +0200

    fix period convert

--- dashboard/backend/management/quotas/periods.py
+++ dashboard/backend/management/quotas/periods.py
@@ -4,6 +4,5 @@ from dashboard.backend.models import PeriodDisplayInformation
 
 def convert_period_enum_to_display_info(period: Period) -> PeriodDisplayInformation:
     return PeriodDisplayInformation(
-        name=period.name,
-        value=period.value,
+        name=period.name
     )
