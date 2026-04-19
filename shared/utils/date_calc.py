f79f72854fed8f18f5891b23d5cfa1a3210d8176
JLZMYD <jlzmyd@inf.elte.hu>
2026-04-19 17:01:17 +0200

    Add docstrings for shared utils

--- shared/utils/date_calc.py
+++ shared/utils/date_calc.py
@@ -3,6 +3,19 @@ from datetime import datetime, timedelta, timezone
 from shared.models.orm_models import Period
 
 def calculate_date_after_period(period: Period, start_date: datetime | None = None, fast_forward: bool = False) -> datetime:
+    """
+    Calculate the next date after a given period from the start date.
+
+    Parameters
+    ----------
+    - period: The period to add from the Period enum.
+    - start_date: The date from which to calculate the next date. If None, the current date and time will be used.
+    - fast_forward: If True, the calculation will fast-forward to the next date reachable from the start date based on the period, skipping periods that have already passed.
+
+    Returns
+    -------
+    - The calculated date after the specified period.
+    """
     now = datetime.now(timezone.utc)
     
     if start_date is None:
--- shared/utils/pathing.py
+++ shared/utils/pathing.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 
 def find_project_root(start: Path = None, marker: str = "shared/") -> Path:
+    """
+    Tries to find the project root directory by looking for a specific marker file or directory (default is "shared/") starting from the given path and moving up the directory tree until it finds the marker or reaches the filesystem root.
+    Used to locate the project root directory inside Docker containers where folder structures may differ.
+
+    Parameters
+    ----------
+    - start: The starting path to begin the search. If None, it defaults to the directory of this file.
+    - marker: The name of the file or directory to look for as an indicator of the project root. Defaults to "shared/".
+
+    Returns
+    -------
+    - Path: The path to the project root directory.
+    """
     if start is None:
         start = Path(__file__).parent
     current = start.resolve()
