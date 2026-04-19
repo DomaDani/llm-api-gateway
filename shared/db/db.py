98d3f0ef6d5da93b74261811272e168207a1b5b0
JLZMYD <jlzmyd@inf.elte.hu>
2026-04-19 16:43:28 +0200

    create docstring for shared db fncs

--- shared/db/db.py
+++ shared/db/db.py
@@ -18,12 +18,26 @@ SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_co
 
 @asynccontextmanager
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provides an asynchronous context manager for obtaining a SQLAlchemy session. It creates a new session using the SessionLocal factory and yields it for use within the context. The session is automatically closed when the context is exited.
+
+    Returns
+    -------
+    - AsyncSession: An asynchronous SQLAlchemy session for interacting with the database.
+    """
     async with SessionLocal() as session:
         yield session
 
 
 @asynccontextmanager
 async def get_transactional_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provides an asynchronous context manager for obtaining a transactional SQLAlchemy session. It creates a new session using the SessionLocal factory and yields it for use within the context. The session is automatically closed when the context is exited.
+
+    Returns
+    -------
+    - AsyncSession: An asynchronous transactional SQLAlchemy session for interacting with the database.
+    """
     async with SessionLocal() as session:
         async with session.begin():
             yield session
